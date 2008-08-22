ff437b10d578afe60588c0b9917dea60f8a653c5
tsmckelvey <tsmckelvey@d1d1f741-0555-0410-b7bf-cb97d25edfb0>
2008-08-22 20:56:45 +0000

    added docblocks
    git-svn-id: http://yadif.googlecode.com/svn/trunk@26 d1d1f741-0555-0410-b7bf-cb97d25edfb0

--- Yadif_Container.php
+++ Yadif_Container.php
@@ -13,10 +13,22 @@
 
 require_once 'Yadif_Exception.php';
 
+/**
+ * Yadif_Container
+ *
+ * @category   Yadif
+ * @copyright  Copyright (c) 2008 Thomas McKelvey
+ */
 class Yadif_Container
 {
+	/**
+	 * Class index key of component $config
+	 */
 	const CONFIG_CLASS = 'class';
 
+	/**
+	 * Arguments index key of component $config
+	 */
 	const CONFIG_ARGUMENTS = 'arguments';
 
 	// container of component configurations
@@ -25,52 +37,86 @@ class Yadif_Container
 	// parameters which have been set, expected to be bound
 	protected $_parameters = array();
 
-	protected function _validateArg($arg, $argVar, $type)
-	{
-		$isTypes = array('array', 'bool', 'float', 'int', 'null', 'numeric', 'object',
-						 'resource', 'scalar', 'string');
-
-		if (in_array($type, $isTypes)) $isType = "is_$type";
-
-		if (!$isType($arg))
-			throw new Yadif_Exception($argVar . ' must be ' . $type . ', is ' . gettype($arg));
-
-		return $arg;
-	}
-
 	public function __construct(array $config = null)
 	{
 		if (isset($array)) {
-			$config = $this->_validateArg($config, '$config', 'array');
+			if (!is_array($config)) throw new Yadif_Exception('$config not array, is ' . gettype($config));
 
 			foreach ($config as $componentName => $componentConfig) {
 				$this->addComponent( $componentName, $componentConfig );
 			}
 		}
 	}
+
+	/**
+	 * Getter method for internal array of component configurations
+	 *
+	 * @return array
+	 */
+	public function getContainer()
+	{
+		return $this->_container();
+	}
+
+	/**
+	 * Getter method for internal array of parameters
+	 * 
+	 * @return array
+	 */
+	public function getParameters()
+	{
+		return $this->_parameters();
+	}
+
+	/**
+	 * Retrieve a parameter by name
+	 *
+	 * @return mixed $param The value of the parameter
+	 */
+	public function getParam($param)
+	{
+		return $this->_parameters[$param];
+	}
 	
+	/**
+	 * Add a component to the container
+	 *
+	 * If $config is omitted, $name is assumed to be the classname.
+	 *
+	 * @param string|Yadif_Container $name Class-tag, class, or Yadif_Container
+	 * @param array $config Array of configuration values
+	 * @return Yadif_Container
+	 */
 	public function addComponent($name = null, array $config = null)
 	{
-		$name = $this->_validateArg($name, '$name', 'string');
+		if (!is_string($name) && !($name instanceof Yadif_Container)) 
+			throw new Yadif_Exception('$string not string|Yadif_Container, is ' . gettype($string));
 
-		$config = $this->_validateArg($config, '$config', 'array');
-
-		if (!class_exists( $config[self::CONFIG_CLASS] ))
-			throw new Yadif_Exception( 'Class ' . $config[self::CONFIG_CLASS] . ' not found' );
+		if (!is_array($config)) { // assume name is the class name
+			$config[self::CONFIG_CLASS] = $name;
+		}
 
 		if (!is_array($config[ self::CONFIG_ARGUMENTS ]))
 			$config[ self::CONFIG_ARGUMENTS ] = array();
+
+		if (!class_exists( $config[self::CONFIG_CLASS] ))
+			throw new Yadif_Exception( 'Class ' . $config[self::CONFIG_CLASS] . ' not found' );
 
 		$this->_container[ $name ] = $config;
 
 		return $this;
 	}
 
-	public function bindParam($param, $value)
+	/**
+	 * Bind a parameter
+	 *
+	 * @param string $param The parameter name, to be given with a leading colon ":param"
+	 * @param mixed $value The value to bind to the parameter
+	 * @return Yadif_Container
+	 */
+	public function bindParam($param = null, $value)
 	{
-		$param = $this->_validateArg($param, '$param', 'string');
-
-		$value = $this->_validateArg($value, '$value', 'string');
+		if (!is_string($param)) throw new Yadif_Exception('$param not string, is ' . gettype($param));
 
 		if ($param{0} != ':')
 			throw new Yadif_Exception($param . ' must start with a colon (:)');
@@ -80,9 +126,15 @@ class Yadif_Container
 		return $this;
 	}
 
+	/**
+	 * Get back a fully assembled component based on the configuration provided beforehand
+	 *
+	 * @param string $name The name of the component
+	 * @return mixed 
+	 */
 	public function getComponent($name = null)
 	{
-		$name = $this->_validateArg($name, '$name', 'string');
+		if (!is_string($name)) throw new Yadif_Exception('$name not string, is ' . gettype($name));
 
 		// if we're trying to "getParameter" (see the loop below)
 		if (array_key_exists($name, $this->_parameters)) {
